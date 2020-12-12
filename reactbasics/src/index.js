ac0363f4c9b8fe570ffbdb463b4a262240c7f7b6
Suzad <61018495+Suzad@users.noreply.github.com>
2020-12-12 08:25:56 +0600

    my trial

---
+++ reactbasics/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <h1>this is my first react project</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
